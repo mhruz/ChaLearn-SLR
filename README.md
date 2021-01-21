2738cb8d17fdfbf189653d3ffc1a71657dafb5c9
Marek Hruz <mhruz@kky.zcu.cz>
2021-01-21 21:24:21 +0100

    hand_grabber hand region extraction

--- README.md
+++ README.md
@@ -2,3 +2,35 @@
 These codes generate the results for 2021 Looking at People Large Scale Signer Independent Isolated SLR CVPR Challenge.
 
 http://chalearnlap.cvc.uab.es/challenge/43/description/
+
+## OpenPose joint indexes
+
+The locations are from the person's point of view (as caputured in video, must not reflect reality, if the video was mirrored).
+E.g. Left shoulder means the 'left' shoulder of the person which is on the right side of the image.
+For hand joints and parts refer to https://www.mdpi.com/sensors/sensors-20-01074/article_deploy/html/images/sensors-20-01074-g001.png
+
+
+Index | Body part | Index | Body part | Index | Body part
+--- | --- | --- | --- | --- | ---
+0 | head | 8 | left hand wrist | 29 | right hand wrist
+1 | neck | 9 | left root of thumb (CMC) | 30 | right root of thumb (CMC)
+2 | right shoulder | 10 | left thumb 1st joint (MCP) | 31 | right thumb 1st joint (MCP)
+3 | right elbow | 11 | left thumb 2nd joint (DIP) | 32 | right thumb 2nd joint (DIP)
+4 | right wrist | 12 | left thumb fingertip | 33 | right thumb fingertip
+5 | left shoulder | 13 | left index finger MCP | 34 | right index finger MCP
+6 | left elbow | 14 | left index finger PIP | 35 | right index finger PIP
+7 | left wrist | 15 | left index finger DIP | 36 | right index finger DIP
+ | | | 16 | left index fingertip | 37 | right index fingertip
+ | | | 17 | left middle finger MCP | 38 | right middle finger MCP
+ | | | 18 | left middle finger PIP | 39 | right middle finger PIP
+ | | | 19 | left middle finger DIP | 40 | right middle finger DIP
+ | | | 20 | left middle fingertip | 41 | right middle fingertip
+ | | | 21 | left ring finger MCP | 42 | right ring finger MCP
+ | | | 22 | left ring finger PIP | 43 | right ring finger PIP
+ | | | 23 | left ring finger DIP | 44 | right ring finger DIP
+ | | | 24 | left ring fingertip | 45 | right ring fingertip
+ | | | 25 | left pinky finger MCP | 46 | right pinky finger MCP
+ | | | 26 | left pinky finger PIP | 47 | right pinky finger PIP
+ | | | 27 | left pinky finger DIP | 48 | right pinky finger DIP
+ | | | 28 | left pinky fingertip | 49 | right pinky fingertip
+
--- open_pose_hand_grabber.py
+++ open_pose_hand_grabber.py
@@ -1,6 +1,63 @@
 import argparse
 import h5py
 import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def draw_joints(im, joints):
+    joints = np.reshape(joints, (-1, 3))
+
+    for i, joint in enumerate(joints):
+        cv2.circle(im, (joint[0], joint[1]), 3, (0, 255, 0), thickness=-1)
+
+
+def get_right_hand(im, joints, border=0.3):
+    joints = np.reshape(joints, (-1, 3))
+    right_hand_joints = joints[29:49]
+
+    hand_image = get_sub_image(im, right_hand_joints, border)
+
+    return hand_image
+
+
+def get_left_hand(im, joints, border=0.3):
+    joints = np.reshape(joints, (-1, 3))
+    left_hand_joints = joints[8:28]
+
+    hand_image = get_sub_image(im, left_hand_joints, border)
+
+    return hand_image
+
+
+def get_sub_image(im, joints, border=0.2):
+    x0 = np.min(joints[:, 0])
+    x1 = np.max(joints[:, 0])
+
+    y0 = np.min(joints[:, 1])
+    y1 = np.max(joints[:, 1])
+
+    width = x1 - x0
+    height = y1 - y0
+
+    border_width = width * border / 2.0
+    border_height = height * border / 2.0
+
+    x0 = np.maximum(0, x0 - border_width)
+    x1 = np.minimum(im.shape[1], x1 + border_width)
+    y0 = np.maximum(0, y0 - border_height)
+    y1 = np.minimum(im.shape[0], y1 + border_height)
+
+    x0 = int(np.round(x0))
+    x1 = int(np.round(x1))
+    y0 = int(np.round(y0))
+    y1 = int(np.round(y1))
+
+    sub_image = im[y0:y1, x0:x1, :]
+
+    return sub_image
+
 
 if __name__ == "__main__":
     # parse commandline
@@ -15,4 +72,39 @@ if __name__ == "__main__":
     video_filenames = os.listdir(args.video_path)
     video_filenames = [x for x in video_filenames if x.endswith("_color.mp4")]
 
+    pause = 40
+
+    cv2.namedWindow("image", 0)
+    cv2.namedWindow("left hand", 0)
+    cv2.namedWindow("right hand", 0)
+
     for video_fn in video_filenames:
+        video = cv2.VideoCapture(os.path.join(args.video_path, video_fn))
+        frame = 0
+
+        while True:
+
+            ret, im = video.read()
+            if not ret:
+                break
+
+            joints = joints_h5[video_fn[:-4]][frame]
+            #draw_joints(im, joints)
+
+            # get the right hand image
+            right_hand_image = get_right_hand(im, joints)
+            # get the left hand image
+            left_hand_image = get_left_hand(im, joints)
+
+            cv2.imshow("left hand", left_hand_image)
+            cv2.imshow("right hand", right_hand_image)
+
+            cv2.imshow("image", im)
+            key = cv2.waitKey(pause)
+            if key == 32:
+                pause = abs(pause - 40)
+
+            frame += 1
+
+
+    cv2.destroyWindow()
