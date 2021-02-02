000244e5132fe434b401278c4d06a7c5bdaaf4e2
marek.hruz <marek.hruz@amitia-ai.com>
2021-02-02 01:26:22 +0100

    classic euclid distance added for highlighting the differences

--- visual_language_embedding/utils.py
+++ visual_language_embedding/utils.py
@@ -5,7 +5,7 @@ from skimage import transform as tf
 import cv2
 
 
-def compute_hand_pose_distance(hp1, hp2):
+def compute_hand_pose_distance(hp1, hp2, metric_one=1):
     tform = tf.estimate_transform('similarity', hp1, hp2)
     hp1_homo = np.hstack((hp1, np.ones((hp1.shape[0], 1))))
     reprojected_hp1 = np.matmul(tform.params[:2, :], hp1_homo.T)
@@ -13,20 +13,27 @@ def compute_hand_pose_distance(hp1, hp2):
     flat_hp1 = reprojected_hp1.T.flatten()
     flat_hp2 = hp2.flatten()
 
-    return numpy.linalg.norm(flat_hp1 - flat_hp2)
+    return numpy.linalg.norm(flat_hp1 - flat_hp2) / metric_one
 
 
 if __name__ == "__main__":
 
     f_joints = h5py.File(r'z:\korpusy_cv\AUTSL\train_json_keypoints-raw.h5', 'r')
-    video_fn_ref = "signer8_sample1857_color.mp4"
-    video_fn_target = "signer26_sample523_color.mp4"
+    video_fn_ref = "signer7_sample1874_color.mp4"
+    video_fn_target = "signer2_sample392_color.mp4"
 
-    ref_idx = 43
+    joints = f_joints[video_fn_ref[:-4]][0]
+    joints = np.reshape(joints, (-1, 3))
+    shoulder_length = np.linalg.norm(joints[1, :2] - joints[2, :2]).item()
+
+    joints_target = f_joints[video_fn_target[:-4]][0]
+    joints_target = np.reshape(joints_target, (-1, 3))
+    shoulder_length_target = np.linalg.norm(joints_target[1, :2] - joints_target[2, :2]).item()
+
+    ref_idx = 26
     joints = f_joints[video_fn_ref[:-4]][ref_idx]
-
-    hp1 = np.reshape(joints, (-1, 3))[8:28]
-    hp1 = hp1[:, :2]
+    joints = np.reshape(joints, (-1, 3))
+    hp1 = joints[8:28, :2]
 
     f_hands = h5py.File(r"z:\korpusy_cv\AUTSL\train_hand_images.h5")
     print(f_hands[video_fn_ref]["left_hand"]["frames"][:])
@@ -36,14 +43,16 @@ if __name__ == "__main__":
         joints = np.reshape(joints, (-1, 3))[8:28]
         hp2 = joints[:, :2]
 
-        dist = compute_hand_pose_distance(hp1, hp2)
+        dist = compute_hand_pose_distance(hp1, hp2, metric_one=shoulder_length)
+        dist_euclid = np.linalg.norm(((hp1 - np.mean(hp1)) / shoulder_length).flatten() -
+                                     ((hp2 - np.mean(hp2)) / shoulder_length_target).flatten())
 
-        print(i, dist, i in f_hands[video_fn_target]["left_hand"]["frames"][:], np.mean(joints[:, 2]))
+        print(i, dist, i in f_hands[video_fn_target]["left_hand"]["frames"][:], np.mean(joints[:, 2]), dist_euclid)
 
     im1_idx = np.where(f_hands[video_fn_ref]["left_hand"]["frames"][:] == ref_idx)
     im1 = f_hands[video_fn_ref]["left_hand"]["images"][im1_idx][0]
 
-    im2_idx = np.where(f_hands[video_fn_target]["left_hand"]["frames"][:] == 44)
+    im2_idx = np.where(f_hands[video_fn_target]["left_hand"]["frames"][:] == 53)
     im2 = f_hands[video_fn_target]["left_hand"]["images"][im2_idx][0]
 
     cv2.namedWindow("im1", 0)
