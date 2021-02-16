4829ac71af18643cc59b410562319b3369241050
marek.hruz <marek.hruz@amitia-ai.com>
2021-02-16 21:24:07 +0100

    train and val set split

--- visual_language_embedding/create_training_set.py
+++ visual_language_embedding/create_training_set.py
@@ -8,22 +8,27 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create training and validation sets of hands.')
     parser.add_argument('hand_crops', type=str, help='path to h5 with hand crop images')
     parser.add_argument('hand_clusters', type=str, help='path to pickle with hand clusters')
-    parser.add_argument('output', type=str, help='path to output h5 dataset')
+    parser.add_argument('train_set', type=str, help='path to output training h5 dataset')
+    parser.add_argument('val_set', type=str, help='path to output validation h5 dataset')
     args = parser.parse_args()
 
     data = pickle.load(open(args.hand_clusters, "rb"))
     f_hand_crops = h5py.File(args.hand_crops, "r")
 
     f_out = h5py.File(args.output, "w")
-    f_out.create_dataset("hand_images", (100, 70, 70, 3), maxshape=(None, 70, 70, 3), dtype=np.uint8)
-    f_out.create_dataset("labels", (100, 1), maxshape=(None, 1), dtype=np.int)
+    f_out.create_dataset("train_images", (100, 70, 70, 3), maxshape=(None, 70, 70, 3), dtype=np.uint8)
+    f_out.create_dataset("val_images", (100, 70, 70, 3), maxshape=(None, 70, 70, 3), dtype=np.uint8)
+    f_out.create_dataset("train_labels", (100, 1), maxshape=(None, 1), dtype=np.int)
+    f_out.create_dataset("val_labels", (100, 1), maxshape=(None, 1), dtype=np.int)
 
     final_clusters = data["hand_clusters"]
     index_to_representative = data["index_to_representative"]
     hand_samples = data["hand_samples"]
     sign_hand_clusters = data["sign_hand_clusters"]
 
-    hand_data_len = 0
+    train_data_len = 0
+    val_data_len = 0
+    val_signers = ["signer40", "signer42"]
     for i, hand_cluster in enumerate(final_clusters):
         for hand_repre in hand_cluster:
             cluster_info = index_to_representative[hand_repre]
@@ -36,17 +41,29 @@ if __name__ == "__main__":
                 img_idx = np.where(f_hand_crops["{}".format(sample)]["left_hand"]["frames"][:] == frame)
                 img = f_hand_crops["{}".format(sample)]["left_hand"]["images"][img_idx][0]
 
-                f_out["hand_images"][hand_data_len, :, :, :] = img
-                f_out["labels"][hand_data_len] = i
+                if any(s in sample for s in val_signers):
+                    f_out["val_images"][val_data_len, :, :, :] = img
+                    f_out["val_labels"][val_data_len] = i
 
-                hand_data_len += 1
+                    val_data_len += 1
 
-                if hand_data_len == len(f_out["labels"]):
-                    f_out["hand_images"].resize((hand_data_len + 100, 70, 70, 3))
-                    f_out["labels"].resize((hand_data_len + 100, 1))
+                    if val_data_len == len(f_out["val_labels"]):
+                        f_out["val_images"].resize((val_data_len + 100, 70, 70, 3))
+                        f_out["val_labels"].resize((val_data_len + 100, 1))
+                else:
+                    f_out["train_images"][train_data_len, :, :, :] = img
+                    f_out["train_labels"][train_data_len] = i
+
+                    train_data_len += 1
+
+                    if train_data_len == len(f_out["labels"]):
+                        f_out["train_images"].resize((train_data_len + 100, 70, 70, 3))
+                        f_out["train_labels"].resize((train_data_len + 100, 1))
 
     # truncate to actual length
-    f_out["hand_images"].resize(hand_data_len, 70, 70, 3)
-    f_out["labels"].resize(hand_data_len, 1)
+    f_out["train_images"].resize(train_data_len, 70, 70, 3)
+    f_out["train_labels"].resize(train_data_len, 1)
+    f_out["val_images"].resize(val_data_len, 70, 70, 3)
+    f_out["val_labels"].resize(val_data_len, 1)
 
     f_out.close()
