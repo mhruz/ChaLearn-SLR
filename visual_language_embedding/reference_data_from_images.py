36321423663c19898eab8898546d6c4ec1895377
Marek Hruz <mhruz@kky.zcu.cz>
2021-02-24 17:41:54 +0100

    minimizing computations

--- visual_language_embedding/reference_data_from_images.py
+++ visual_language_embedding/reference_data_from_images.py
@@ -56,12 +56,28 @@ if __name__ == "__main__":
                         help='maximal distance between hand poses considered to be the same hand pose', default=0.6)
     parser.add_argument('--min_conf', type=float,
                         help='minimal accepted confidence of joint estimation', default=0.7)
+    parser.add_argument('--max_samples', type=int,
+                        help='maximum number of reference images (if there is more, the reference class is ignored)',
+                        default=1000)
     parser.add_argument('--hand_crops', type=str, help='optional h5 with cropped images of hands')
     parser.add_argument('output', type=str, help='path to output with clustered hand poses')
     args = parser.parse_args()
 
     f_joints = h5py.File(args.joints_h5, "r")
+    joints_data = {}
+    for video_fn in f_joints:
+        joints_data[video_fn] = f_joints[video_fn][:]
+
     f_signs = h5py.File(args.sign_clusters_h5, "r")
+    sign_clusters_data = {}
+    for sign_class in f_signs:
+        sign_clusters_data[sign_class] = {}
+        sign_clusters_data[sign_class]["samples"] = f_signs[sign_class]["samples"][:]
+        sign_clusters_data[sign_class]["frames"] = f_signs[sign_class]["frames"][:]
+        sign_clusters_data[sign_class]["seeders"] = {}
+        sign_clusters_data[sign_class]["seeders"]["samples"] = f_signs[sign_class]["seeders"]["samples"][:]
+        sign_clusters_data[sign_class]["seeders"]["frames"] = f_signs[sign_class]["seeders"]["frames"][:]
+
     if args.hand_crops is not None:
         f_hand_crops = h5py.File(args.hand_crops, "r")
 
@@ -92,20 +108,32 @@ if __name__ == "__main__":
 
     all_ref_samples = []
     candidates = {}
+    hand_pose_classes_valid = []
     for hand_class in hand_pose_classes:
-        candidates[hand_class] = []
         images_class = os.listdir(os.path.join(args.data_root, hand_class))
-        for image_fn in images_class:
-            all_ref_samples.append(image_fn)
+        images_class = [x for x in images_class if x.endswith(".jpg")]
+        if len(images_class) < args.max_samples:
+            candidates[hand_class] = []
+            hand_pose_classes_valid.append(hand_class)
+            for image_fn in images_class:
+                all_ref_samples.append(image_fn)
+
+    hand_pose_classes = hand_pose_classes_valid
+    reference_hands = {}
 
     for hand_class in hand_pose_classes:
         print("Processing hand class: {}".format(hand_class))
-        images_class = os.listdir(os.path.join(args.data_root, hand_class))
-        for image_fn in images_class:
-            sample_ref = image_fn[:image_fn.find("_color")+6]
-            frame_ref = int(image_fn[image_fn.find("_color")+6:-4])
 
-            joints_ref = f_joints[sample_ref][frame_ref]
+        if hand_class not in reference_hands:
+            reference_hands[hand_class] = []
+
+        images_class = os.listdir(os.path.join(args.data_root, hand_class))
+        images_class = [x for x in images_class if x.endswith(".jpg")]
+        for image_fn in images_class:
+            sample_ref = image_fn[:image_fn.find("_color") + 6]
+            frame_ref = int(image_fn[image_fn.find("_color") + 6:-4])
+
+            joints_ref = joints_data[sample_ref][frame_ref]
             joints_ref = np.reshape(joints_ref, (-1, 3))
 
             hand_ref = joints_ref[8:29][:, :2]
@@ -114,11 +142,30 @@ if __name__ == "__main__":
             if hand_ref_conf < args.min_conf:
                 continue
 
-            img_idx = np.where(f_hand_crops["{}".format(sample_ref)]["left_hand"]["frames"][:] == frame_ref)
-            img_ref = f_hand_crops["{}".format(sample_ref)]["left_hand"]["images"][img_idx][0]
+            new_hand = True
+            for ref_hand in reference_hands[hand_class]:
+                dist = compute_hand_pose_distance_weighted(ref_hand, hand_ref, shoulder_ref)
+                if dist < args.max_distance / 2:
+                    new_hand = False
+                    break
 
-            for sign_class in f_signs:
-                for frame, sample in zip(f_signs[sign_class]["frames"], f_signs[sign_class]["samples"]):
+            if new_hand:
+                reference_hands[hand_class].append(hand_ref)
+            else:
+                continue
+
+            # img_idx = np.where(f_hand_crops["{}".format(sample_ref)]["left_hand"]["frames"][:] == frame_ref)
+            # img_ref = f_hand_crops["{}".format(sample_ref)]["left_hand"]["images"][img_idx][0]
+
+            ignored_sign_classes = list(range(35))
+            ignored_sign_classes.extend(list(range(150, 163)))
+            ignored_sign_classes.extend(list(range(200, 216)))
+            for sign_class in sign_clusters_data:
+                if sign_class in ignored_sign_classes:
+                    continue
+
+                for frame, sample in zip(sign_clusters_data[sign_class]["frames"],
+                                         sign_clusters_data[sign_class]["samples"]):
                     sample = sample.decode("utf-8")
                     if sample == sample_ref and frame == frame_ref:
                         continue
@@ -126,7 +173,7 @@ if __name__ == "__main__":
                     if "{}{}.jpg".format(sample, frame) in all_ref_samples:
                         continue
 
-                    joints = f_joints[sample][frame]
+                    joints = joints_data[sample][frame]
                     joints = np.reshape(joints, (-1, 3))
 
                     hand = joints[8:29][:, :2]
@@ -161,17 +208,17 @@ if __name__ == "__main__":
                         if not found_same:
                             candidates[hand_class].append({"sample": sample_string, "distance": dist})
 
-                        img_idx = np.where(f_hand_crops["{}".format(sample)]["left_hand"]["frames"][:] == frame)
-                        img = f_hand_crops["{}".format(sample)]["left_hand"]["images"][img_idx][0]
-
-                        print(dist)
-                        print("ref_conf: {}, target_conf: {}".format(hand_ref_conf, hand_conf))
-
-                        cv2.namedWindow("ref", 2)
-                        cv2.namedWindow("close", 2)
-                        cv2.imshow("ref", img_ref)
-                        cv2.imshow("close", img)
-                        cv2.waitKey()
-                        cv2.destroyAllWindows()
+                        # img_idx = np.where(f_hand_crops["{}".format(sample)]["left_hand"]["frames"][:] == frame)
+                        # img = f_hand_crops["{}".format(sample)]["left_hand"]["images"][img_idx][0]
+                        #
+                        # print(dist)
+                        # print("ref_conf: {}, target_conf: {}".format(hand_ref_conf, hand_conf))
+                        #
+                        # cv2.namedWindow("ref", 2)
+                        # cv2.namedWindow("close", 2)
+                        # cv2.imshow("ref", img_ref)
+                        # cv2.imshow("close", img)
+                        # cv2.waitKey()
+                        # cv2.destroyAllWindows()
 
     pickle.dump(candidates, open("candidates.p", "wb"))
