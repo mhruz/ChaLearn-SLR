38848b2aabf9296cd991e62c1878813d958d273d
Matyáš Boháček <matyas.bohacek@matsworld.io>
2021-02-25 13:55:21 +0100

    Modularize the ChaLearn challenge data processing methods

--- mbohacek/fav_disc_space_management.py
+++ mbohacek/fav_disc_space_management.py
@@ -3,126 +3,156 @@ import paramiko
 import h5py
 import os
 import PIL.Image
+import logging
+import statistics
 
 import pandas as pd
 
 
-data_file = h5py.File("/Users/matyasbohacek/Documents/Academics/Materials/CVPR SLR ChaLearn/Data/train_json_keypoints-raw.h5", "r")
+class ChaLearnDataManager:
 
-df = pd.read_csv("/Users/matyasbohacek/Documents/Academics/Materials/CVPR SLR ChaLearn/Data/labels_jpg.csv", encoding="utf-8", sep=" ", header=None)[30000:100000]
-df.columns = ["img_file", "vid_file", "frame_index", "label", "signer"]
+    def __init__(self, keypoints_data_file_path, labels_info_df_path, enable_ssh=False, img_path_store=""):
+        """
+        Initializes the ChaLearnDataManager for processing of the data associated with the ChaLearn competition.
 
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname="", username="mbohacek")
+        :param keypoints_data_file_path: Path to the HDF5 file with the body pose landmarks from OpenPose for all the
+                                         sign instances
+        :param labels_info_df_path: Path to the CSV table with the properties of all the sign instances and
+                                    corresponding frames
+        :param enable_ssh: Determines whether a connection should be made to the NTIS server for potential data
+                           downloading
+        :param img_path_store: Path into which the downloaded frames' images should be saved
+        """
 
+        self.keypoints_data_file = h5py.File(keypoints_data_file_path, "r")
 
-def download_image(remote_name: str, local_name: str):
-    """
-    Downloads the given image from the AUTSL dataset's training image folder on the disc server.
+        self.labels_info_df = pd.read_csv(labels_info_df_path, encoding="utf-8", sep=" ", header=None)
+        self.labels_info_df.columns = ["img_file", "vid_file", "frame_index", "label", "signer"]
 
-    :param remote_name: Name of the remote file (image)
-    :param local_name: Name for local saving
-    """
+        self.ssh_enabled = enable_ssh
+        self.img_path_store = img_path_store
+        if enable_ssh:
+            self.__setup_ssh_client()
 
-    ftp_client = ssh_client.open_sftp()
-    ftp_client.get("/data-ntis/projects/korpusy_cv/AUTSL/train_jpg/" + remote_name, "img_transfered/" + local_name)
+    def __setup_ssh_client(self):
+        """
+        Creates a SSH connection to NTIS
+        """
 
-    ftp_client.close()
+        self.ssh_client = paramiko.SSHClient()
+        self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh_client.connect(hostname="", username="mbohacek")
 
+    def download_image(self, remote_name: str, local_name: str):
+        """
+        Downloads the given image from the AUTSL dataset's training image folder on the disc server.
 
-def get_saved_landmarks(sign_sample_id: str, frame: int, convert_to_coordinates: bool = False, normalize: bool = False,
-                        width: int = 0, height: int = 0):
-    """
-    Process the OpenCV body landmarks saved in the .h5 file.
+        :param remote_name: Name of the remote file (image)
+        :param local_name: Name for local saving
+        """
 
-    :param sign_sample_id: Identifier of the specific sign video sample
-    :param frame: Frame index
-    :param convert_to_coordinates: Determines whether the data should be converted into coordinates (50, 2) or left in
-           the original format along with the confidence (150)
-    :param normalize: Determines whether to normalize the landmarks according to the image's shape (the data will be
-           relative)
-    :param width: Width of the image (necessary only if `normalize`==True)
-    :param height: Height of the image (necessary only if `normalize`==True)
-    :return: Returns the specified landmarks (either as np.array or list of tuples, according to
-             `convert_to_coordinates`)
-    """
+        if not self.ssh_enabled:
+            logging.warning("The SSH connection was not enabled. Downloading aborted.")
+            return
 
-    # Structure only the raw sample name, if given with video extension
-    if sign_sample_id.endswith(".mp4"):
-        sign_sample_id = sign_sample_id.replace(".mp4", "")
+        ftp_client = self.ssh_client.open_sftp()
+        ftp_client.get("/data-ntis/projects/korpusy_cv/AUTSL/train_jpg/" + remote_name,
+                       os.path.join(self.img_path_store, local_name))
+        ftp_client.close()
 
-    result = data_file[sign_sample_id][frame]
+    def get_saved_landmarks(self, sign_sample_id: str, frame: int, convert_to_coordinates: bool = False, normalize: bool = False,
+                            width: int = 0, height: int = 0):
+        """
+        Process the OpenCV body landmarks saved in the .h5 file.
 
-    # Normalize the landmarks positions according to the image if specified
-    if normalize:
-        if width == 0 or height == 0:
-            print("Could not normalize due to the parameters not being passed.")
-        else:
-            result[::3] /= width
-            result[1::3] /= height
+        :param sign_sample_id: Identifier of the specific sign video sample
+        :param frame: Frame index
+        :param convert_to_coordinates: Determines whether the data should be converted into coordinates (50, 2) or left in
+               the original format along with the confidence (150)
+        :param normalize: Determines whether to normalize the landmarks according to the image's shape (the data will be
+               relative)
+        :param width: Width of the image (necessary only if `normalize`==True)
+        :param height: Height of the image (necessary only if `normalize`==True)
+        :return: Returns the specified landmarks (either as np.array or list of tuples, according to
+                 `convert_to_coordinates`)
+        """
 
-    # Convert array into a list of tuples if specified
-    if convert_to_coordinates:
-        result = list(result)
+        # Structure only the raw sample name, if given with video extension
+        if sign_sample_id.endswith(".mp4"):
+            sign_sample_id = sign_sample_id.replace(".mp4", "")
 
-        x = result[::3]
-        y = result[1::3]
+        result = self.keypoints_data_file[sign_sample_id][frame]
 
-        result = list(zip(x, y))
+        # Normalize the landmarks positions according to the image if specified
+        if normalize:
+            if width == 0 or height == 0:
+                print("Could not normalize due to the parameters not being passed.")
+            else:
+                result[::3] /= width
+                result[1::3] /= height
 
-    return result
+        # Calculate the average hand confidence
+        average_confidence = statistics.mean(result[2::3][8:50])
 
+        # Convert array into a list of tuples if specified
+        if convert_to_coordinates:
+            result = list(result)
 
-def download_and_process_frame(sign_sample_id: str, frame: int, convert_to_coordinates: bool = False, normalize: bool =
-                               False) -> (str, PIL.Image, list):
-    """
-    Combines and orchestrates the necessary processes to process landmarks of the given frame from the desired video
-    sample. If the frame is not present, it is downloaded.
+            x = result[::3]
+            y = result[1::3]
 
-    :param sign_sample_id: Identifier of the specific sign video sample
-    :param frame: Frame index
-    :param convert_to_coordinates: Determines whether the data should be converted into coordinates (50, 2) or left in
-           the original format along with the confidence (150)
-    :param normalize: Determines whether to normalize the landmarks according to the image's shape (the data will be
-           relative)
-    :return: Relative path of the image, PIL image, landmarks data
-    """
+            result = list(zip(x, y))
 
-    img_name = sign_sample_id.replace(".mp4", "") + "_" + str(frame) + ".jpg"
+        return result, average_confidence
 
-    if not os.path.isfile("img_transfered/" + img_name):
-        download_image(df[(df["vid_file"] == sign_sample_id) & (df["frame_index"] == frame)]["img_file"].tolist()[0] ,img_name)
+    def download_and_process_frame(self, sign_sample_id: str, frame: int, convert_to_coordinates: bool = False, normalize: bool =
+                                   False) -> (str, PIL.Image, list):
+        """
+        Combines and orchestrates the necessary processes to process landmarks of the given frame from the desired video
+        sample. If the frame is not present, it is downloaded.
 
-    img = PIL.Image.open("img_transfered/" + img_name)
-    width, height = img.size
+        :param sign_sample_id: Identifier of the specific sign video sample
+        :param frame: Frame index
+        :param convert_to_coordinates: Determines whether the data should be converted into coordinates (50, 2) or left in
+               the original format along with the confidence (150)
+        :param normalize: Determines whether to normalize the landmarks according to the image's shape (the data will be
+               relative)
+        :return: Relative path of the image, PIL image, landmarks data
+        """
 
-    landmarks_data = get_saved_landmarks(sign_sample_id, frame, convert_to_coordinates, normalize, width, height)
+        img_name = sign_sample_id.replace(".mp4", "") + "_" + str(frame) + ".jpg"
 
-    return "img_transfered/" + img_name, img, landmarks_data
+        if not os.path.isfile("img_transfered/" + img_name):
+            self.download_image(self.labels_info_df[(self.labels_info_df["vid_file"] == sign_sample_id) & (self.labels_info_df["frame_index"] == frame)]["img_file"].tolist()[0] ,img_name)
 
+        img = PIL.Image.open("img_transfered/" + img_name)
+        width, height = img.size
 
-def fav_format_to_structured(landmarks) -> (list, list):
-    """
-    Converts the format in which the training data is saved into the separated standardized format for the pipeline.
+        landmarks_data, average_confidence = self.get_saved_landmarks(sign_sample_id, frame, convert_to_coordinates, normalize, width, height)
 
-    :param landmarks: List of the landmarks
-    :return: Body landmarks (18, 2), hands landmarks (2, 21, 2)
+        return "img_transfered/" + img_name, img, landmarks_data
 
-    :warn: The landmarks data should be normalized and in the converted format with shape (50, 2), not (150)
-    """
+    def fav_format_to_structured(self, landmarks) -> (list, list):
+        """
+        Converts the format in which the training data is saved into the separated standardized format for the pipeline.
 
-    body = [None] * 18
+        :param landmarks: List of the landmarks
+        :return: Body landmarks (18, 2), hands landmarks (2, 21, 2)
 
-    for body_index in range(8):
-        body[body_index] = landmarks[body_index]
+        :warn: The landmarks data should be normalized and in the converted format with shape (50, 2), not (150)
+        """
 
-    hands = [[None] * 21] * 2
+        body = [None] * 18
 
-    hands[0] = landmarks[8:29]
-    hands[1] = landmarks[29:50]
+        for body_index in range(8):
+            body[body_index] = landmarks[body_index]
 
-    return body, hands
+        hands = [[None] * 21] * 2
+
+        hands[0] = landmarks[8:29]
+        hands[1] = landmarks[29:50]
+
+        return body, hands
 
 
 if __name__ == "__main__":
