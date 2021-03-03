a3db2b47f1ddc90d308e9928c085ca8fe506b65a
marek.hruz <marek.hruz@amitia-ai.com>
2021-03-03 13:51:06 +0100

    location + vle

---
+++ semantic_vector_utils.py
@@ -0,0 +1,58 @@
+import h5py
+import numpy as np
+
+
+def get_semantic_vector_location_vle(location_vectors, vle_data):
+    sample_length = len(location_vectors)
+    embedding_dim = vle_data["left_hand"]["embeddings"].shape[1]
+
+    output = np.zeros((sample_length, location_vectors.shape[1] * location_vectors.shape[2] + 2 * embedding_dim))
+
+    # tak the first embedding as the last known hand
+    # if there is no embedding for the sample, set it to zeros (fallback)
+    if len(vle_data["left_hand"]["embeddings"]) >= 0:
+        known_left_hand = vle_data["left_hand"]["embeddings"][0]
+    else:
+        known_left_hand = np.zeros((1, embedding_dim))
+
+    if len(vle_data["right_hand"]["embeddings"]) >= 0:
+        known_right_hand = vle_data["right_hand"]["embeddings"][0]
+    else:
+        known_right_hand = np.zeros((1, embedding_dim))
+
+    # iterate through the frames of the sample
+    for frame in range(sample_length):
+        # set the location vectors (left_hand, right_hand)
+        output[frame, 0:15] = location_vectors[frame][0]
+        output[frame, 15:30] = location_vectors[frame][1]
+
+        # set the embeddings
+        if frame in vle_data["left_hand"]["frames"]:
+            vle_index = np.where(vle_data["left_hand"]["frames"][:] == frame)[0]
+            output[frame, 30:30 + 1280] = vle_data["left_hand"]["embeddings"][vle_index]
+
+            known_left_hand = vle_data["left_hand"]["embeddings"][vle_index]
+        # if the current frame is not present in the embeddings data, use the last known embedding
+        else:
+            output[frame, 30:30 + 1280] = known_left_hand
+
+        if frame in vle_data["right_hand"]["frames"]:
+            vle_index = np.where(vle_data["right_hand"]["frames"][:] == frame)[0]
+            output[frame, 30 + 1280:30 + 2560] = vle_data["right_hand"]["embeddings"][vle_index]
+
+            known_right_hand = vle_data["right_hand"]["embeddings"][vle_index]
+        else:
+            output[frame, 30 + 1280:30 + 2560] = known_right_hand
+
+    return output
+
+
+if __name__ == "__main__":
+    loc_vectors = h5py.File(r"z:\korpusy_cv\AUTSL\location_vectors.h5", "r")
+    vle_data = h5py.File(r"z:\korpusy_cv\AUTSL\vle_hand_crops_train.h5", "r")
+
+    sample = "signer0_sample1_color"
+
+    data = get_semantic_vector_location_vle(loc_vectors[sample], vle_data[sample])
+
+    print("Done")
