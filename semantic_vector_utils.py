567858316c82802a13abd5258033a8be0a86fdf0
marek.hruz <marek.hruz@amitia-ai.com>
2021-03-03 22:43:23 +0100

    data to memory

--- semantic_vector_utils.py
+++ semantic_vector_utils.py
@@ -51,16 +51,19 @@ def get_semantic_vector_location_vle_keyframes(location_vectors, vle_data, keyfr
     sample_length = len(keyframes)
     embedding_dim = vle_data["left_hand"]["embeddings"].shape[1]
 
+    # hdf5 to numpy
+    location_vectors = location_vectors[:, :, :]
+
     output = np.zeros((sample_length, location_vectors.shape[1] * location_vectors.shape[2] + 2 * embedding_dim))
 
     # tak the first embedding as the last known hand
     # if there is no embedding for the sample, set it to zeros (fallback)
-    if len(vle_data["left_hand"]["embeddings"]) >= 0:
+    if len(vle_data["left_hand"]["embeddings"]) > 0:
         known_left_hand = vle_data["left_hand"]["embeddings"][0]
     else:
         known_left_hand = np.zeros((1, embedding_dim))
 
-    if len(vle_data["right_hand"]["embeddings"]) >= 0:
+    if len(vle_data["right_hand"]["embeddings"]) > 0:
         known_right_hand = vle_data["right_hand"]["embeddings"][0]
     else:
         known_right_hand = np.zeros((1, embedding_dim))
@@ -103,12 +106,29 @@ def get_semantic_vector_location_vle_keyframes(location_vectors, vle_data, keyfr
 
 if __name__ == "__main__":
     loc_vectors = h5py.File(r"z:\korpusy_cv\AUTSL\location_vectors.h5", "r")
-    vle_data = h5py.File(r"z:\korpusy_cv\AUTSL\vle_hand_crops_train.h5", "r")
+    vle_h5 = h5py.File(r"z:\korpusy_cv\AUTSL\vle_hand_crops_train.h5", "r")
     keyframes = h5py.File(r"z:\cv\ChaLearnLAP\key_frames_16.h5", "r")
 
-    sample = "signer0_sample1_color"
+    loc_vectors_data = {}
+    vle_data = {}
+    keyframes_data = {}
 
-    #data = get_semantic_vector_location_vle(loc_vectors[sample], vle_data[sample])
-    data = get_semantic_vector_location_vle_keyframes(loc_vectors[sample], vle_data[sample], keyframes[sample])
+    for sample in loc_vectors:
+        loc_vectors_data[sample] = loc_vectors[sample][:]
+        vle_data[sample] = {}
+        vle_data[sample]["left_hand"] = {}
+        vle_data[sample]["left_hand"]["frames"] = vle_h5[sample]["left_hand"]["frames"][:]
+        vle_data[sample]["left_hand"]["embeddings"] = vle_h5[sample]["left_hand"]["embeddings"][:]
+
+        vle_data[sample]["right_hand"] = {}
+        vle_data[sample]["right_hand"]["frames"] = vle_h5[sample]["right_hand"]["frames"][:]
+        vle_data[sample]["right_hand"]["embeddings"] = vle_h5[sample]["right_hand"]["embeddings"][:]
+
+        keyframes_data[sample] = keyframes[sample][:]
+
+    sample = "signer0_sample1_color"
+    # data = get_semantic_vector_location_vle(loc_vectors[sample], vle_data[sample])
+    data = get_semantic_vector_location_vle_keyframes(loc_vectors_data[sample], vle_data[sample],
+                                                      keyframes_data[sample])
 
     print("Done")
