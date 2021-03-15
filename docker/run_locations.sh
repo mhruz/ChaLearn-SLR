17a00d7dce50dbe2a2a1e2aeae37d699b64a72f5
marek.hruz <marek.hruz@amitia-ai.com>
2021-03-15 15:34:26 +0100

    run_locations.sh

---
+++ docker/run_locations.sh
@@ -0,0 +1,1 @@
+python -m mbohacek.annotations_script --video_directory /home/data/test/ --keypoints_datafile /home/data/test_json_keypoints-raw.h5 --labels_info /home/data/test_labels.csv --output_file /home/data/location_vectors_test.h5 --logging normal
