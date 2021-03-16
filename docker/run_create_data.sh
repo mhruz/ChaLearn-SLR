5ab2f7f204a75b6536c9c4b43afcc9d39d289304
zdkrnoul <47319584+zdkrnoul@users.noreply.github.com>
2021-03-16 11:29:39 +0100

    Update run_create_data.sh

--- docker/run_create_data.sh
+++ docker/run_create_data.sh
@@ -1,4 +1,4 @@
 # generate test hand crop images
-python open_pose_hand_grabber.py /home/data/test /home/data/test_json_keypoints-raw.h5 --out_h5 test_hand_images.h5
+python open_pose_hand_grabber.py /home/data/test /home/data/test_json_keypoints-raw.h5 --out_h5 /home/data/test_hand_images.h5
 # generate test key-frames
 python key_frame_extractor.py /home/data/test_json_keypoints-raw.h5 16 /home/data/test_key_frames_16.h5
