4ad2d4a1b2f029febb52101615f6964b75b7169b
zdkrnoul <47319584+zdkrnoul@users.noreply.github.com>
2021-03-16 12:26:12 +0100

    Update run_locations.sh

--- docker/run_locations.sh
+++ docker/run_locations.sh
@@ -1,1 +1,6 @@
-python -m mbohacek.annotations_script --video_directory /home/data/test/ --keypoints_datafile /home/data/test_json_keypoints-raw.h5 --labels_info /home/data/test_labels.csv --output_file /home/data/location_vectors_test.h5 --logging normal
+#Location Vectors
+python -m mbohacek.annotations_script --video_directory /home/data/test/ \
+        --keypoints_datafile /home/data/test_json_keypoints-raw.h5 \
+        --labels_info /home/data/test_labels.csv \
+        --output_file /home/data/location_vectors_test.h5 \
+        --logging normal
