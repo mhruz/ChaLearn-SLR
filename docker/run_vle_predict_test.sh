448c12221c1b73663706dfa605d57d70441bc341
zdkrnoul <47319584+zdkrnoul@users.noreply.github.com>
2021-03-16 12:27:26 +0100

    Update run_vle_predict_test.sh

--- docker/run_vle_predict_test.sh
+++ docker/run_vle_predict_test.sh
@@ -1,10 +1,11 @@
-#create VLE test input
+#create VLE input
 run_create_data.sh
 
-#predict test VLE
+#predict test VLE Vectors
 python /home/experiment/visual_language_embedding/predict.py /home/data/test_hand_images.h5 \
        /home/data/models/vle/vle_mobilenet_pretrained_normalized_resized_aug_ref_images_39.pthepoch_9.tar \
        mobilenet 39 /home/data/vle_hand_crops_test_v2.h5 --open_pose_h5 /home/data/train_json_keypoints-raw.h5 --resize 224 --min_conf 0.55
        
-#create Locations
+#create Locations Vectors
+run_locations.sh
 
