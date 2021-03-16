7e9f3bb93efb30a0767a19b42421282dae4deac1
zdkrnoul <47319584+zdkrnoul@users.noreply.github.com>
2021-03-16 12:00:15 +0100

    Update run_pred_4.sh

--- transformer-vle/run_pred_4.sh
+++ transformer-vle/run_pred_4.sh
@@ -24,3 +24,5 @@ python3 ./chainer-transformer/train_hpoes.py --train-pred pred \
                                              --debug-data 0 \
                                              --resume "" \
                                              --model-name $OUTPUTDIR/vle_04
+                                             
+mv $WORKINGDIR/vle.csv  /home/data/test_csv/vle_04.csv                                             
